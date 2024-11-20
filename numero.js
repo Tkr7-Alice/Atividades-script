94176b4b2efc783555eb98f72530445a727f7371
ThomasKirmeier <thomas.kirmeier@gmail.com>
2024-11-19 21:47:29 -0300

    Add files via upload

---
+++ numero.js
@@ -0,0 +1,18 @@
+const btn = window.document.getElementById('btn1');
+
+const saida = window.document.getElementById('saida');
+
+function exibirMensagem(mensagem){
+    saida.textContent = mensagem;
+}
+
+btn.addEventListener('click', function(){
+    let num = window.prompt('Digite um número');
+
+    if(num % 2 == 0){
+        exibirMensagem('É par'); 
+    }else {
+        exibirMensagem('É impar');
+    }
+}
+);
---
+++ script.js
@@ -0,0 +1,15 @@
+function contar(){
+    let contador = 0;
+    let res = document.getElementById('resultado');
+}
+
+function contar(){
+        contador = contador + 1;
+        res.innerHTML = '<p>O contador está com <mark>${contador}>/mark></p>';
+    }
+function zerar(){
+    contador = 0;
+    res.innerHTML = `<p>O contador está`
+}       
+
+
